601109de2854301c1e77e95df181e2b5ab768870
doubledead <jaa2015@live.com>
2016-06-27 17:08:27 -0500

    More CRUD functionality built out for user profile.

--- flaskproject/entries/views.py
+++ flaskproject/entries/views.py
@@ -19,70 +19,11 @@ def index():
 
 @entries.route('/')
 @login_required
-@cache.cached(300)
 def display_entries():
     entries = [entry for entry in Entry.query.all()]
     current_app.logger.info('Displaying all entries.')
 
     return render_template("entries/entries.html", entries=entries)
-
-@entries.route('/<entry_id>', methods=['GET', 'POST'])
-@login_required
-@cache.cached(300)
-def show(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-
-    return render_template("entries/show.html", entry=entry)
-
-@entries.route('/edit/<entry_id>', methods=['GET', 'POST'])
-@login_required
-def update(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-
-    user_id = current_user.id
-    form = UpdateEntryForm()
-    if request.method == "POST" and form.validate_on_submit():
-        entry.title = form.title.data
-        entry.body = form.body.data
-        entry.user_id = user_id
-        current_app.logger.info('Updating entry %s.', entry.title)
-        # entry = Entry(title, body, user_id)
-
-        try:
-            # db.session.add(entry)
-            db.session.commit()
-            # return update(entry)
-        except exc.SQLAlchemyError as e:
-            current_app.logger.error(e)
-
-            # return redirect(url_for('entries.show', entry_id=entry.id))
-        return redirect(url_for('entries.display_entries'))
-    else:
-        form.title.data = entry.title
-        form.body.data = entry.body
-
-    return render_template("entries/edit.html", entry=entry, form=form)
-
-@entries.route('/delete/<entry_id>', methods=['GET', 'POST'])
-@login_required
-def delete(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
-    user_id = current_user.id
-    if user_id == entry.user_id:
-        current_app.logger.info('Deleting entry %s.', entry.title)
-        try:
-            db.session.delete(entry)
-            db.session.commit()
-        except exc.SQLAlchemyError as e:
-            current_app.logger.error(e)
-
-        return redirect(url_for('entries.display_entries'))
-
-    # db.session.delete(entry)
-    # db.session.commit()
-
-    return redirect(url_for('entries.display_entries'))
-
 
 @entries.route('/create', methods=['GET', 'POST'])
 @login_required
@@ -109,4 +50,56 @@ def create_entry():
         return redirect(url_for('entries.display_entries'))
 
     return render_template("entries/create_entry.html", form=form)
+
+@entries.route('/<entry_id>', methods=['GET', 'POST'])
+@login_required
+def show(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+
+    return render_template("entries/show.html", entry=entry)
+
+@entries.route('/edit/<entry_id>', methods=['GET', 'POST'])
+@login_required
+def update(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+
+    user_id = current_user.id
+    form = UpdateEntryForm()
+    if request.method == "POST" and form.validate_on_submit():
+        entry.title = form.title.data
+        entry.body = form.body.data
+        entry.user_id = user_id
+        current_app.logger.info('Updating entry %s.', entry.title)
+
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            current_app.logger.error(e)
+
+        return redirect(url_for('entries.show', entry_id=entry.id))
+    else:
+        form.title.data = entry.title
+        form.body.data = entry.body
+
+    return render_template("entries/edit.html", entry=entry, form=form)
+
+@entries.route('/delete/<entry_id>', methods=['GET', 'POST'])
+@login_required
+def delete(entry_id):
+    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+    user_id = current_user.id
+    if user_id == entry.user_id:
+        current_app.logger.info('Deleting entry %s.', entry.title)
+        try:
+            db.session.delete(entry)
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            current_app.logger.error(e)
+
+        return redirect(url_for('entries.display_entries'))
+
+    # db.session.delete(entry)
+    # db.session.commit()
+
+    return redirect(url_for('entries.display_entries'))
 
--- flaskproject/users/views.py
+++ flaskproject/users/views.py
@@ -15,26 +15,36 @@ user = Blueprint('user', __name__, template_folder='templates')
 def index():
     return render_template('users/user_profile.html')
 
+@user.route('/<user_id>', methods=['GET', 'POST'])
+@login_required
+def show(user_id):
+    user_id = current_user.id
+    user = User.query.filter_by(id=user_id).first_or_404()
+
+    return render_template('users/user_profile.html', user=user)
+
 @user.route('/edit', methods=['GET', 'POST'])
 @login_required
-def edit():
+def update():
+    user_id = current_user.id
+    user = User.query.filter_by(id=user_id).first_or_404()
     form = EditProfileForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        last_edit_date = datetime.utcnow()
-        current_app.logger.info('Saving profile information for %s.', (email))
-        user = User(email, password, last_edit_date)
+
+    # if request.method == "POST" and form.validate_on_submit():
+    if request.method == "POST":
+        user.email = form.email.data
+        user.password = form.password.data
+        user.last_edit_date = datetime.utcnow()
+        current_app.logger.info('Saving profile information for %s.', user.email)
 
         try:
-            db.session.add(user)
             db.session.commit()
         except exc.SQLAlchemyError as e:
             current_app.logger.error(e)
 
-            return redirect(url_for('edit'))
+        return redirect(url_for('user.show', user_id=user.id))
     else:
-        form.email.data = current_user.email
-        form.password.data = current_user.password
+        form.email.data = user.email
+        form.password.data = user.password
 
-    return render_template('users/edit_profile.html', form=form)
+    return render_template('users/edit_profile.html', user=user, form=form)
