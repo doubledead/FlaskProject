d2f5c7c9500b74ec9dd459e1c2c5d6cf4fd1c0d3
doubledead <jaa2015@live.com>
2016-06-30 15:10:11 -0500

    Added momentjs to Bower dependencies.

--- flaskproject/entries/forms.py
+++ flaskproject/entries/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import Form
 from wtforms import StringField, TextAreaField
 from wtforms import validators
-# from wtforms import TextField, TextAreaField, validators
 
 class CreateEntryForm(Form):
     # title = TextField('Title', [validators.Length(min=1, max=70)])
--- flaskproject/events/forms.py
+++ flaskproject/events/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import StringField, DateField
+from wtforms.fields import DateField, DateTimeField
 # from wtforms.validators import DataRequired
 from wtforms import validators
-# from wtforms import Form, TextField, TextAreaField, validators
 
 class CreateEventForm(Form):
     title = StringField('Title', [validators.Length(min=1, max=70)])
