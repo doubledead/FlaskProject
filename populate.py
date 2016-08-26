75187c1b313a26ffabfce069a6fd8636f09decf5
doubledead <jaa2015@live.com>
2016-08-26 13:55:43 -0500

    Styling updates. Added Bower dependency to static vendor directories.

--- populate.py
+++ populate.py
@@ -3,8 +3,8 @@ from flaskproject.core import db
 from flaskproject.events.models import Event, Category, Status
 
 def populate_status():
-    status_active = Status(name='active', description='active item', status_code=001)
-    status_inactive = Status(name='inactive', description='inactive item', status_code=002)
+    status_active = Status(name='active', description='active item', status_code=100)
+    status_inactive = Status(name='inactive', description='inactive item', status_code=200)
 
     db.session.add(status_active)
     db.session.add(status_inactive)
